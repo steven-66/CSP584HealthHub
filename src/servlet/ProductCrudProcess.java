3b9b3ed92d3125d9a5a9b9a4f6a6421eb9534c82
Haoli Yang <hyang77@hawk.iit.edu>
2020-12-02 01:22:26 -0600

    Work on CRUD logic

--- src/servlet/ProductCrudProcess.java
+++ src/servlet/ProductCrudProcess.java
@@ -3,6 +3,7 @@ package servlet;
 import java.io.IOException;
 import java.util.*;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,24 +17,31 @@ import bean.Product;
 public class ProductCrudProcess extends HttpServlet {
     static ProductCrudService productCrudService = new ProductCrudService();
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String method = request.getParameter("method");
-        if (method.equals("delete")) {
-            deleteProduct(request, response);
-        }
-    }
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
-        addProduct(request, response);
+        doGet(request, response);
     }
 
-    private void deleteProduct(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String productId = request.getParameter("id");
-        productCrudService.delete(productId);
-        response.sendRedirect("admin.jsp");
+        String action = request.getParameter("action");
+        if (action.equals("showNewForm")) {
+            showNewForm(request, response);
+        } else if (action.equals("addProduct")) {
+            addProduct(request, response);
+        } else if (action.equals("deleteProduct")) {
+            deleteProduct(request, response);
+        } else if (action.equals("showEditForm")) {
+            showEditForm(request, response);
+        } else if (action.equals("updateProduct")) {
+            updateProduct(request, response);
+        }
+
+    }
+
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("productForm.jsp").forward(request, response);
     }
 
     private void addProduct(HttpServletRequest request, HttpServletResponse response)
@@ -47,17 +55,45 @@ public class ProductCrudProcess extends HttpServlet {
         String productDiscount = request.getParameter("discount");
         String productCatagory = request.getParameter("catagory");
         String productInventory = request.getParameter("inventory");
-        Product product = new Product();
-        product.setId(productId);
-        product.setName(productName);
-        product.setPrice(Double.parseDouble(productPrice));
-        product.setImage(productImage);
-        product.setManufacturer(productBrand);
-        product.setCondition(productCondition);
-        product.setDiscount(Double.parseDouble(productDiscount));
-        product.setCatagory(productCatagory);
-        product.setInventory(Integer.parseInt(productInventory));
+        Product product = new Product(productId, productName, Double.parseDouble(productPrice), productImage,
+                productBrand, productCondition, Double.parseDouble(productDiscount), productCatagory,
+                Integer.parseInt(productInventory));
         productCrudService.add(product);
         response.sendRedirect("admin.jsp");
     }
+
+    private void deleteProduct(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String productId = request.getParameter("id");
+        productCrudService.delete(productId);
+        response.sendRedirect("admin.jsp");
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String productId = request.getParameter("id");
+        Product existingProduct = productCrudService.getProductObjById(productId);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("productForm.jsp");
+        request.setAttribute("product", existingProduct);
+        dispatcher.forward(request, response);
+    }
+
+    private void updateProduct(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String productId = request.getParameter("productId");
+        String productName = request.getParameter("productName");
+        String productPrice = request.getParameter("productPrice");
+        String productImage = request.getParameter("productImage");
+        String productBrand = request.getParameter("manufacturer");
+        String productCondition = request.getParameter("condition");
+        String productDiscount = request.getParameter("discount");
+        String productCatagory = request.getParameter("catagory");
+        String productInventory = request.getParameter("inventory");
+        Product product = new Product(productId, productName, Double.parseDouble(productPrice), productImage,
+                productBrand, productCondition, Double.parseDouble(productDiscount), productCatagory,
+                Integer.parseInt(productInventory));
+        productCrudService.update(product);
+        response.sendRedirect("admin.jsp");
+    }
+
 }
